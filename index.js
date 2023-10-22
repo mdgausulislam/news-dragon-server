b87f5262eb209aa8d43a84f07eb9be8a2a7ff6f1
unknown <gausulislam5@gmail.com>
2023-10-22 12:26:56 +0600

    update server side data handeling

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port=process.env.PORT || 5000;
 app.use(cors());
 
 const categories=require('./data/category.json')
+const news=require('./data/news.json')
 
 
 app.get('/',(req,res)=>{
@@ -13,6 +14,33 @@ app.get('/',(req,res)=>{
 app.get('/category',(req,res)=>{
     res.send(categories);
 })
+
+app.get('/news',(req,res)=>{
+    res.send(news);
+})
+
+app.get('/news/:id',(req,res)=>{
+    const id= req.params.id;
+    console.log(id);
+    const selectedNews=news.find(n=>n._id===id);
+    res.send(selectedNews)
+})
+
+
+app.get('/categories/:id',(req,res)=>{
+    const id= parseInt(req.params.id);
+    console.log(id);
+
+    if(id===0){
+        res.send(news)
+    }
+    else{
+        const categoryNews=news.filter(n=>parseInt(n.category_id)===id)
+        res.send(categoryNews)
+    }
+})
+
+
 app.listen(port,()=>{
     console.log(`dragon api is running on port:${port}`);
 })
